9b9cdadc03f07409297bc87546c4ae9ae8051e20
Jeff Vincent <jdv@Jeffs-Mac-mini.local>
2020-10-09 22:10:49 -0700

    Add optional VM sort feature

--- create_ansible_inventory.py
+++ create_ansible_inventory.py
@@ -3,45 +3,75 @@ import ast
 import os
 import subprocess
 
+#########Available sort_key values:####################
+#######################################################
+# owner,virtual_machine_name,virtual_machine_id,
+# node_location,node_status,virtual_machine_ip,
+# vnc_port,screen_sharing_port,ssh_port,cpu,
+# vcpu,RAM,base_image,image,
+# configuration_template,vm_status 
+
 
 class CreateOrkaAnsibleInventory:
 
     def __init__(self, output_dir):
-        self.raw_data = None
+        self.vm_data = None
+        self.sorted_data = None
         self.output_dir = output_dir
+        self.connection_info = ('[all:vars]\nanisble_connection=ssh\n'
+                                'ansible_ssh_user=<ssh user>\n'
+                                'ansible_ssh_pass=<ssh password>\n\n'
+                                '[hosts]\n')
+
 
     def get_current_vm_data(self):
         completed_process = subprocess.run(
             ['orka', 'vm', 'list', '--json'], 
             capture_output=True)
         dict_string = completed_process.stdout.decode('utf-8')
-        raw_data = ast.literal_eval(dict_string)
-        self.raw_data = raw_data
+        data = ast.literal_eval(dict_string)
+        self.vm_data = data['virtual_machine_resources']
 
-    def write_inventory(self):
+
+    def sort_vm_data(self, sort_key):
+        self.sorted_data = sorted(
+            self.vm_data, key = lambda vm: vm['status'][0][sort_key])
+
+
+    def write_inventory(self, data):
         inventory_path = os.path.join(self.output_dir, 'inventory')
-        connection_info = ('[all:vars]\nanisble_connection=ssh\n'
-                            'ansible_ssh_user=<ssh user>\n'
-                            'ansible_ssh_pass=<ssh password>\n\n'
-                            '[hosts]\n')
         with open(inventory_path, 'w+') as f:
-            f.write(connection_info)
-            for vm in self.raw_data['virtual_machine_resources']:
-                line = '{}   ansible_ssh_port={}  ansible_ssh_host={}'.format(
+            f.write(self.connection_info)
+            for vm in data:
+                line = '{}   ansible_ssh_port={}   ansible_ssh_host={}'.format(
                     vm['virtual_machine_name'],
                     vm['status'][0]['ssh_port'],
                     vm['status'][0]['virtual_machine_ip'])
                 f.write(line + '\n')
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('output_dir', help='The path to the directory \
+    parser.add_argument('output_dir', 
+        help='The path to the directory \
         where the inventory file will be written.')
+    parser.add_argument(
+        '--sort-key', 
+        action='store', 
+        dest='sort_key', 
+        help='Optional. The key in the VM dict by which \
+        to sort the VMs.')
     args = parser.parse_args()
     output_dir = args.output_dir
     inventory_creator = CreateOrkaAnsibleInventory(output_dir)
     inventory_creator.get_current_vm_data()
-    inventory_creator.write_inventory()
+    if args.sort_key:
+        inventory_creator.sort_vm_data(args.sort_key)
+        inventory_creator.write_inventory(
+            inventory_creator.sorted_data)
+    else:
+        inventory_creator.write_inventory(
+            inventory_creator.vm_data)
 
 
 
