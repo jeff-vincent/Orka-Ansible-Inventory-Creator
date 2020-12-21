037aed95d83127478ebfd0e64b1b45c7e6331987
Jeff Vincent <jeff.d.vincent@gmail.com>
2020-12-20 19:03:51 -0800

    Add --host arg handling

--- orka_inventory.py
+++ orka_inventory.py
@@ -42,6 +42,15 @@ class OrkaAnsibleInventory:
             [i for i in self.vm_data if i['vm_deployment_status'] == 'Deployed']
 
 
+    def get_vm_by_host_name(self, host_name):
+        """Filter current VM data to isolate named VM.
+        Args:
+            host_name: string: the VM name to match.
+        """
+        self.filtered_data = \
+            [i for i in self.vm_data if host_name in i['status'][0]['virtual_machine_name']]
+
+
     def get_name_contains_vms(self, name_contains):
         """Filter current VM data to isolate VMs by partial name match.
         Args:
@@ -88,11 +97,19 @@ class OrkaAnsibleInventory:
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--list', help='list deployed VMs',
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('--list', help='list deployed VMs',
                     action='store_true')
+    group.add_argument('--host', help='get host by name', action='store',
+                    dest='host_name')
     args = parser.parse_args()
     name_contains = os.environ.get('ANSIBLE_NAME_CONTAINS')
-
+    if args.host_name:
+        host_name = args.host_name
+        inventory_creator = OrkaAnsibleInventory()
+        inventory_creator.get_current_vm_data()
+        inventory_creator.get_vm_by_host_name(host_name)
+        inventory_creator.create_inventory()
     if args.list:
         inventory_creator = OrkaAnsibleInventory()
         inventory_creator.get_current_vm_data()
